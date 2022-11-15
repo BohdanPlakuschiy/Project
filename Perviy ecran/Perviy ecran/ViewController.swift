5e896d9a9c4722dd1d9b40fc46fac1a880800d59
Bohdan Plakuschiy <plakushiy.bogdan@gmail.com>
2022-11-15 11:45:47 +0200

    New project

---
+++ Perviy ecran/Perviy ecran/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Perviy ecran
+//
+//  Created by Богдан Плакущий on 15.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
