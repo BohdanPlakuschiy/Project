12298b0337f740c939da601c1ebe3b7abf9f608f
Bohdan Plakuschiy <plakushiy.bogdan@gmail.com>
2022-11-15 14:09:12 +0200

    no message

--- Perviy ecran/Perviy ecran/ViewController.swift
+++ Perviy ecran/Perviy ecran/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstImage: UIImageView!
+    @IBOutlet weak var secondImage: UIImageView!
+    @IBOutlet weak var thirdImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func turnOffTheFirstImage(_ sender: Any) {
+    }
+    @IBAction func turnOffTheSecondImage(_ sender: Any) {
+    }
+    @IBAction func turnOffTheThirdImage(_ sender: Any) {
+    }
+    
+    @IBAction func showAll(_ sender: Any) {
+    }
+    
 }
 
